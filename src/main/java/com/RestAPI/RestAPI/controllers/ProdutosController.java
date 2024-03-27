ac9d75af7537cd5a661776953279058528c4d46a
Rafael Azevedo <rafael.azevedo.dev@gmail.com>
2024-03-27 15:19:15 -0300

    COMMIT 1.CRIADA REQUISICAO PUT PARA ATUALIZACAO/ALTERACAO DE PRODUTOS SEGUINDO OS PADROES DE UMA API REST
    Signed-off-by: Rafael Azevedo <rafael.azevedo.dev@gmail.com>

--- src/main/java/com/RestAPI/RestAPI/controllers/ProdutosController.java
+++ src/main/java/com/RestAPI/RestAPI/controllers/ProdutosController.java
@@ -4,7 +4,6 @@ import com.RestAPI.RestAPI.dtos.ProdutosDTO;
 import com.RestAPI.RestAPI.models.ProdutosModelo;
 import com.RestAPI.RestAPI.repositories.ProdutoRepositorio;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,6 +42,18 @@ public class ProdutosController {
     public ResponseEntity<Object> listarProdutosPorId(@PathVariable(value="id") String id){
         Optional<ProdutosModelo> listaProdutos = produtoRepositorio.findById(id);
         return ResponseEntity.status(HttpStatus.OK).body(listaProdutos);
+    }
+
+    @PutMapping("/produtos/{id}")
+    public ResponseEntity<Object> alterarProduto(@PathVariable (value="id") String id,
+                                                         @RequestBody @Valid ProdutosDTO produtoDTO){
+        Optional<ProdutosModelo> produto0 = produtoRepositorio.findById(id);
+        if(produto0.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("PRODUTO NÃO ENCONTRADO");
+        }
+        ProdutosModelo modeloProduto = produto0.get();
+        BeanUtils.copyProperties(produtoDTO, modeloProduto);
+        return ResponseEntity.status(HttpStatus.OK).body(produtoRepositorio.save(modeloProduto));
     }
 
 }
